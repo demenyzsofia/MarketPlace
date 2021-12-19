e7bc04ec310a977b613790b45d60f883b3d91159
demenyzsofia <79308025+demenyzsofia@users.noreply.github.com>
2021-12-19 15:04:45 +0200

    Correction of fragment_product_detail.xml

--- app/src/main/java/com/example/marketplace/fragments/ProductDetailFragment.kt
+++ app/src/main/java/com/example/marketplace/fragments/ProductDetailFragment.kt
@@ -6,9 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.marketplace.R
 import com.example.marketplace.models.Product
 import com.example.marketplace.repository.Repository
@@ -16,11 +19,16 @@ import com.example.marketplace.viewmodels.products.ListViewModel
 import com.example.marketplace.viewmodels.products.ListViewModelFactory
 
 class ProductDetailFragment : Fragment() {
-    lateinit var productImage : ImageView
-    lateinit var productName : TextView
-    lateinit var productSeller : TextView
-    lateinit var productDescription : TextView
-    lateinit var productPrice : TextView
+    private lateinit var productImage : ImageView
+    private lateinit var productName : TextView
+    private lateinit var productSeller : TextView
+    private lateinit var productDescription : TextView
+    private lateinit var productPrice : TextView
+    private lateinit var isActive : TextView
+    private lateinit var availableAmount : TextView
+    private lateinit var orderButton: Button
+    private lateinit var emailButton: Button
+    private lateinit var phoneButton: Button
 
     val factory = ListViewModelFactory(Repository())
     private val listViewModel: ListViewModel by lazy{
@@ -47,13 +55,32 @@ class ProductDetailFragment : Fragment() {
     }
 
     private fun registerListeners(view: View) {
-//        val product: Product? = listViewModel.getOneProductMyList()
        val product: Product? = listViewModel.getOneProduct()
 
         productDescription.setText(product?.description)
-        productPrice.setText(product?.price_per_unit + " RON")
+        productPrice.setText(product?.price_per_unit + " " + product?.price_type + "/" + product?.amount_type)
         productSeller.setText(product?.username)
         productName.setText(product?.title)
+        if(product?.is_active.toString() == "true"){
+            isActive.text = "Active"
+        }
+        else{
+            isActive.text = "Inactive"
+        }
+        availableAmount.setText("Available amount: " + product?.units+product?.amount_type)
+
+        orderButton.setOnClickListener(){
+            findNavController().navigate(R.id.action_productDetailFragment_to_orderDialog)
+        }
+
+        emailButton.setOnClickListener(){
+            Toast.makeText(context , "Messaging doesn't work!", Toast.LENGTH_SHORT).show()
+        }
+
+        phoneButton.setOnClickListener(){
+            Toast.makeText(context , "The call cannot be made!", Toast.LENGTH_SHORT).show()
+        }
+
 
 
     }
@@ -64,6 +91,11 @@ class ProductDetailFragment : Fragment() {
         productPrice = view.findViewById(R.id.textView_product_price)
         productDescription = view.findViewById(R.id.textView_product_description)
         productImage = view.findViewById(R.id.imageView_product)
+        isActive = view.findViewById(R.id.textView_product_isActive)
+        availableAmount = view.findViewById(R.id.textView_product_availableAmount)
+        orderButton = view.findViewById(R.id.button_order)
+        emailButton = view.findViewById(R.id.button_message)
+        phoneButton = view.findViewById(R.id.button_phone)
     }
 
 }
